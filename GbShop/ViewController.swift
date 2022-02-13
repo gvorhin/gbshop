be0de69eb2f37a87ae9eaf33632059562aa7815f
Igor <Igor>
2022-02-13 15:10:20 +0300

    initial commit

---
+++ GbShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GbShop
+//
+//  Created by i.vasilyev on 13.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
